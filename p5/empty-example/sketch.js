52864e758e07f9a7afa2ae1c46c10e05fd96c5b1
Guilherme de Lyra <guilyra12@gmail.com>
2019-04-03 20:04:17 -0300

    funções de remoção de vertices e arestas

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -25,25 +25,24 @@ function setup() {
   slider = createSlider(1, 300, 100);
   sliderValue = createP(slider.value());
 
-  sliderValue.parent('#zoom');
-  slider.parent('#zoom');
+  sliderValue.parent("#zoom");
+  slider.parent("#zoom");
   //sliderValue.style("","");
-
 
   grid = new Grid(100);
   graph = new Graph();
   graph.createGraph();
 
-  addButton = createButton('Adicionar Vértice');
-  inputX = createInput('Posição X');
-  inputY = createInput('Posição Y');
-  addButton.parent('#addVertice');
-  inputX.parent('addVertice');
-  inputY.parent('addVertice');
+  addButton = createButton("Adicionar Vértice");
+  inputX = createInput("Posição X");
+  inputY = createInput("Posição Y");
+  addButton.parent("#addVertice");
+  inputX.parent("addVertice");
+  inputY.parent("addVertice");
 
-  addButton.mousePressed(function(){
-    graph.addVertex((graph.qtdVertex+1),inputX.value(),inputY.value());
-    while(i<graph.qtdVertex){
+  addButton.mousePressed(function() {
+    graph.addVertex(inputX.value(), inputY.value());
+    while (i < graph.qtdVertex) {
       selectVertex1.option(i);
       selectVertex2.option(i);
       i++;
@@ -55,24 +54,43 @@ function setup() {
   addAresta = createButton("Adicionar Aresta");
   selectVertex1.option("Selecione");
   selectVertex2.option("Selecione");
-  addAresta.parent('addAresta');
-  selectVertex1.parent('addAresta');
-  selectVertex2.parent('addAresta');
+  addAresta.parent("addAresta");
+  selectVertex1.parent("addAresta");
+  selectVertex2.parent("addAresta");
 
-  addAresta.mousePressed(function(){
+  addAresta.mousePressed(function() {
     graph.addLink(selectVertex1.value(), selectVertex2.value());
   });
 
-  
+  selectVertex1 = createSelect();
+  selectVertex2 = createSelect();
+  removeEdge = createButton("Remover Aresta");
+  selectVertex1.option("Selecione");
+  selectVertex2.option("Selecione");
+  removeEdge.parent("removeEdge");
+  selectVertex1.parent("removeEdge");
+  selectVertex2.parent("removeEdge");
 
-  graph.addVertex(0, 100, 100);
-  graph.addVertex(1, -100, 150);
-  graph.addVertex(2, 0, .100);
+  removeEdge.mousePressed(function() {
+    graph.deleteEdge(selectVertex1.value(), selectVertex2.value());
+  });
+
+  removeButton = createButton("Remover Vértice");
+  delInput = createInput("Index");
+  removeButton.parent("#removeVertice");
+  delInput.parent("removeVertice");
+  removeButton.mousePressed(function() {
+    graph.deleteVertex(delInput.value());
+  });
+
+  graph.addVertex(100, 100);
+  graph.addVertex(-100, 150);
+  graph.addVertex(0, 0.1);
 
   graph.addLink(0, 1);
   graph.addLink(2, 0);
 
-  jsScreen.parent('canvas');
+  jsScreen.parent("canvas");
   framerateText = createP(frameRate());
 }
 
@@ -85,16 +103,8 @@ function draw() {
   grid.show();
   graph.show();
 
-  for(vertex in graph.adjList){
-    let vertexPosX = graph.adjList[vertex].posX;
-    let vertexPosY = graph.adjList[vertex].posY;
-    textAlign(CENTER,CENTER);
-    text(vertex,vertexPosX,vertexPosY);
-  }
-
   framerateText.html("fps: " + frameRate());
   sliderValue.html("Zoom :" + slider.value() + "%");
-
 }
 
 function Grid(scale) {
@@ -102,8 +112,16 @@ function Grid(scale) {
 
   this.show = () => {
     this.zoomScale = 1 / zoom;
-    for (var x = -width * (1 / this.zoomScale); x <= width * (1 / this.zoomScale); x += this.scale) {
-      for (var y = -height * (1 / this.zoomScale); y <= height * (1 / this.zoomScale); y += this.scale) {
+    for (
+      var x = -width * (1 / this.zoomScale);
+      x <= width * (1 / this.zoomScale);
+      x += this.scale
+    ) {
+      for (
+        var y = -height * (1 / this.zoomScale);
+        y <= height * (1 / this.zoomScale);
+        y += this.scale
+      ) {
         stroke(100, 100, 100, 10);
         strokeWeight(1);
         line(x, 0, x, height * (1 / this.zoomScale));
@@ -112,13 +130,10 @@ function Grid(scale) {
         line(0, y, -width * (1 / this.zoomScale), y);
       }
     }
-  }
-
+  };
 }
 
-
 function Graph() {
-
   this.qtdVertex = 0;
 
   function Vertex(positionX, positionY) {
@@ -131,47 +146,70 @@ function Graph() {
   }
 
   this.createGraph = () => {
-    this.adjList = new Array();
-  }
+    this.adjList = new Map();
+  };
 
-  this.addVertex = (index, positionX, positionY) => {
+  this.addVertex = (positionX, positionY) => {
     //Adiciona o vertice depois de fazer todas as verificaçoes
-    let vertex = new Vertex(positionX, positionY);    
-    this.adjList.push(vertex);
+    let vertex = new Vertex(positionX, positionY);
+    this.adjList.set(this.qtdVertex, vertex);
     this.qtdVertex = this.qtdVertex + 1;
-  }
+  };
+
+  this.deleteVertex = key => {
+    key = parseInt(key);
+    this.adjList.delete(key);
+    for (var vertex of this.adjList.keys()) {
+      var idx = this.adjList.get(vertex).links.indexOf(key);
+      this.adjList.get(vertex).links.splice(idx, 1);
+    }
+  };
+
+  this.deleteEdge = (key1, key2) => {
+    key1 = parseInt(key1);
+    key2 = parseInt(key2);
+
+    var idx1 = this.adjList.get(key1).links.indexOf(key2);
+    var idx2 = this.adjList.get(key2).links.indexOf(key1);
+
+    this.adjList.get(key1).links.splice(idx1, 1);
+    this.adjList.get(key2).links.splice(idx2, 1);
+  };
 
   this.show = () => {
     var radius = 30;
-    for (vertex in this.adjList) {
-     // console.log('ue' + this.adjList[vertex].posX);
-      vertexPosX = this.adjList[vertex].posX;
-      vertexPosY = this.adjList[vertex].posY;
-      
+    // var pairs = new Set();
+    // var cnt = 0;
 
-      for (link in this.adjList[vertex].links) {
-    //    console.log('ma rapa'+JSON.stringify(this.adjList[vertex].links));
-    //    console.log('oxi'+link);
-        var linkedVPosX = this.adjList[link].posX;
-        var linkedVPosY = this.adjList[link].posY;
-        strokeWeight(3);
-        stroke(100);
-        line(vertexPosX, vertexPosY, linkedVPosX, linkedVPosY);
-      }
+    for (var vertex of this.adjList.keys()) {
+      vertexPosX = this.adjList.get(vertex).posX;
+      vertexPosY = this.adjList.get(vertex).posY;
 
       strokeWeight(3);
       stroke(100);
-      ellipse(vertexPosX, vertexPosY, radius, radius);
-      noStroke();
-     
 
-      
-      
+      for (var link of this.adjList.get(vertex).links) {
+        // var arr = new Array([vertex, link]);
+        // arr.sort()
+        // console.log('entrou' + arr);
+        // if (pairs.has(arr) == false) {
+        // cnt++;
+        var linkedVPosX = this.adjList.get(link).posX;
+        var linkedVPosY = this.adjList.get(link).posY;
+        line(vertexPosX, vertexPosY, linkedVPosX, linkedVPosY);
+        //   pairs.add(arr);
+        // }
+      }
+
+      ellipse(vertexPosX, vertexPosY, radius, radius);
+      textAlign(CENTER, CENTER);
+      noStroke();
+      text(vertex, vertexPosX, vertexPosY);
     }
-  }
+  };
 
   this.addLink = (vertex, linkedVertex) => {
-    this.adjList[vertex].links.push(linkedVertex);
-    this.adjList[linkedVertex].links.push(vertex);
-  }
+    this.adjList.get(vertex).links.push(linkedVertex);
+    this.adjList.get(linkedVertex).links.push(vertex);
+  };
 }
